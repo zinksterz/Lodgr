07b090cdda2c1b4aad1f35f4fd5b6973a2c1ea1a
zinksterz <zehngameplay@gmail.com>
2024-09-13 14:33:10 -0500

    Refamiliarizing myself with the super basics

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+
+function FizzBuzz() {
+let f = 0;
+let b = 0;
+let fb = 0;
+    for (let i = 1; i <= 100; i++){
+        if (i % 3 == 0 && i % 5 == 0){
+            console.log(i + "FizzBuzz\n");
+            fb++;
+        }
+        else if (i % 3 == 0) {
+            console.log(i + "Fizz\n");
+            f++;
+        }
+        else if (i % 5 == 0) {
+            console.log(i + "Buzz\n");
+            b++;
+        }
+        else{
+        console.log(i);
+        }
+    }
+    console.log("Fizz-es: " + f);
+    console.log("Buzz-es: " + b);
+    console.log("FizzBuzz-es: " + fb);
+}
+FizzBuzz();
